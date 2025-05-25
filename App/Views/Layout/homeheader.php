f958e3224fc68a18bc30c4b34732ae35f7e886c0
HuGoTran-dev <tranngochoanganh06@gmail.com>
2025-05-25 23:34:22 +0700

    fix css $ image

--- App/Views/Layout/homeheader.php
+++ App/Views/Layout/homeheader.php
@@ -97,7 +97,7 @@ $assets = $config['assets'];
 
 
     <!-- Header-->
-    <header class="bg-dark py-5">
+    <header class="custom-header py-5">
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
                 <h1 class="display-4 fw-bolder">Shop For Terraria Wolrd</h1>
--- App/Views/home.php
+++ App/Views/home.php
@@ -3,7 +3,7 @@ include_once 'Layout/homeheader.php';
 ?>
 
     <!-- Section: Products -->
-    <section class="py-5">
+    <section class="bg-light py-5">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <?php foreach ($productList as $product): ?>
