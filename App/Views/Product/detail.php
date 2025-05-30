dbc9c6c2e71894d734e5f501aa457bc8bb3956d8
HuGoTran-dev <tranngochoanganh06@gmail.com>
2025-05-30 13:29:10 +0700

    backup pull

--- App/Views/Product/detail.php
+++ App/Views/Product/detail.php
@@ -7,8 +7,11 @@
             <img src="<?= $assets . $product['Image'] ?>" class="img-fluid" alt="<?= $product['Name'] ?>">
         </div>
         <div class="col-md-6">
-            <p><strong>Giá:</strong> <?= number_format($product['Price'], 3, ',', '.') ?> VNĐ</p>
-            <p><strong>Mã sản phẩm:</strong> <?= $product['Id'] ?></p>
+            <h4><strong>Giá:</strong> <?= number_format($product['Price'], 3, ',', '.') ?> VNĐ</h4>
+            <h4><strong>Mã sản phẩm:</strong> <?= $product['Id'] ?></h4>
+            <h5><strong>Mô tả sản phẩm:</strong> <?= $product['Detail'] ?> </h5>
+            <h5><strong>Mô tả sản phẩm:</strong> <?= $product['Detail'] ?> </h5>
+
             <form method="post" action="<?= $baseURL ?>cart/add">
                 <input type="hidden" name="product_id" value="<?= $product['Id'] ?>">
                 <button type="submit" class="btn btn-primary">Thêm vào giỏ</button>
